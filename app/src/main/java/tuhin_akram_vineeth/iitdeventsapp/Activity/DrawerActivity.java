6c6ec5c0c05f35c73434cc5c78f0bf928ff4d339
Tuhinanksu <tuhindas95@gmail.com>
2018-05-03 08:35:28 +0530

    first commit

---
+++ app/src/main/java/tuhin_akram_vineeth/iitdeventsapp/Activity/DrawerActivity.java
@@ -0,0 +1,12 @@
+package tuhin_akram_vineeth.iitdeventsapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/tuhin_akram_vineeth/iitdeventsapp/fragment/SavedFragment.java
@@ -0,0 +1,8 @@
+package tuhin_akram_vineeth.iitdeventsapp.fragment;
+
+/**
+ * Created by TukaiTulu on 03-05-2018.
+ */
+
+public class FeedFragment {
+}
